bc43d53ddbdcc0448048002f7cd0ed3cffe295af
manvantar <manukvshetty@gmail.com>
2021-06-09 21:11:46 +0530

    feat: adding router API Post/add to createContact

---
+++ app/routes/addressBook.js
@@ -0,0 +1,19 @@
+const contactController = require('../controllers/addressBook.js');
+
+module.exports = (app) => {
+
+    // Create a new contact
+    app.post('/add', contactController.create);
+
+    /*// Retrieve all contacts
+    app.get('/contacts',  contactController.findAllContacts);
+
+    // Retrieve a single contact with contactId
+    app.get('/contacts/:contactId',  contactController.findOneData);
+
+    // Update a contact with contactId
+    app.put('/update/:contactId',  contactController.update);
+
+    // Delete a contact with contactId
+    app.delete('/delete/:contactId',  contactController.delete);*/
+}
