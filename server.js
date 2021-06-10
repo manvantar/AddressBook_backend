ac51645ed903817d7d82b1de070b6cde1a1280a7
manvantar <manukvshetty@gmail.com>
2021-06-10 17:36:42 +0530

    feat: adding swagger Documentation for the AddressBook APIs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config(); 
 const express = require('express');
 const dbconnect = require('./config/database.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 // create express app
 const app = express();
@@ -13,6 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(express.json());
+
+//swagger requests to app
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //api for welcome message
 app.get('/', (req, res) => {
