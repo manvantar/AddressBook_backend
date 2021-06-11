95807c608304ed4a51a4c2ca489894468a08c65a
manvantar <manukvshetty@gmail.com>
2021-06-11 12:13:25 +0530

    test: adding tests cases for POST - add/user and add/addressBook

--- test/addressBook.js
+++ test/addressBook.js
@@ -4,11 +4,37 @@ const server = require("../server");
 chai.should();
 chai.use(chaiHttp);
 const fs = require('fs');
-let rawdata = fs.readFileSync('test/contactsData.json');
-let contactJSON = JSON.parse(rawdata);
+let rawdata = fs.readFileSync('test/addressBook.json');
+let addressBookJSON = JSON.parse(rawdata);
 
+describe("POST /add/user", () =>{
+    it("givenNewInputBody_WhenLoggedIn_shouldReturnStatus=200,Success=true", (done) => {
+        const inputBody = addressBookJSON.loginData1;
+        chai.request(server)
+            .post("/add/user")
+            .send(inputBody)
+            .end((error, response) => {
+                response.should.have.status(201);
+                response.body.should.have.property('success').eq(true);
+                response.body.should.have.property('message').eq("User Data Inserted successfully");
+                done();
+            });
+    })
+
+    it("givenNewInputBody_WhenLoggedIn_shouldReturnStatus=500,Success=false", (done) => {
+        const inputBody2 = addressBookJSON.loginData2;
+        chai.request(server)
+            .post("/add/user")
+            .send(inputBody2)
+            .end((error, response) => {
+                response.should.have.status(500);
+                response.body.should.have.property('success').eq(false);
+                done();
+            });
+    })
+})
 describe("GET /", () => {
-    it("given new inputBody When loggedIn should return status 200 with welcome message", (done) => {
+    it("givenNewInputBody_whenLoggedIn_shouldReturnStatus200WithWelcomeMessage", (done) => {
         chai.request(server)
             .get("/")
             .end((error, response) => {
@@ -19,12 +45,30 @@ describe("GET /", () => {
     })
 })
 
-describe("POST /add", () => {
-    it("given new contact in body When added should return status=201 sucess=true", (done) => {
+let jwToken='';
+   
+beforeEach(done => {
+    chai
+        .request(server)
+        .post("/login/user")
+        .send(addressBookJSON.loginData3)
+        .end((err, res) => {
+            jwToken = res.body.token;
+            res.should.have.status(200);
+            done();
+        });
+});
+
+let invalidToken=jwToken.slice(12);
+let empToken='';
+
+describe("POST /add/addressBook", () => {
+    it("givenNewContactInBody_whenAdded_shouldReturnStatus=201Sucess=true", (done) => {
       
         chai.request(server)
-            .post("/add")
-            .send(contactJSON.contact1)
+            .post("/add/addressBook")
+            .send(addressBookJSON.contact1)
+            .set('Authorization', 'Bearar ' + jwToken)
             .end((error, response) => {
                 response.should.have.status(201);
                 response.body.should.have.property('success').eq(true);
@@ -32,13 +76,13 @@ describe("POST /add", () => {
             });
     })
 
-    it("given contact with duplicate number in body When added should return status=500 sucess=false", (done) => {
-       
+    it("givenDuplicateContactInBody_whenAdded_shouldReturnStatus=500Sucess=false", (done) => {
+        
          chai.request(server)
-             .post("/add")
-             .send(contactJSON.contact1)
+             .post("/add/addressBook")
+             .send(addressBookJSON.contact1)
              .end((error, response) => {
-                 response.should.have.status(500);
+                 response.should.have.status(401);
                  response.body.should.have.property('success').eq(false);
                  done();
              });
