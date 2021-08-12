80e76e82abafeabab9c64215f0d1676e05bdf37c
manvantar <manukvshetty@gmail.com>
2021-08-12 12:52:02 +0530

    refactor: surround methods of addressbook services with try catch

--- app/services/addressBook.js
+++ app/services/addressBook.js
@@ -1,70 +1,88 @@
-const logger = require('../../config/logger.js');
-const contactModel = require('../models/addressBook.js');
+const logger = require("../../config/logger.js");
+const contactModel = require("../models/addressBook.js");
 
 class ContactService {
-
-    /**
-    * @description Create method of Model is called to save the new contact Data, Which also encrypts the password
-    * @param newData is data sent from Controller
-    * @return callback is used to callback Controller
-    */
-    create = (newData, callback) => {
-        contactModel.create(newData, (error, data) => {
-            if(error){
-                logger.error("addressBook Create service ->",error);
-                return callback(error, null);  
-            }
-                logger.info("addressBook Create service ->",data);
-                return callback(null, data);
-            // return (error) ? callback(error, null) : callback(null, data);
-        })
+  /**
+   * @description Create method of Model is called to save the new contact Data, Which also encrypts the password
+   * @param newData is data sent from Controller
+   * @return callback is used to callback Controller
+   */
+  create = (newData, callback) => {
+    try {
+      contactModel.create(newData, (error, data) => {
+        if (error) {
+          logger.error("addressBook Create service ->", error);
+          return callback(error, null);
+        }
+        logger.info("addressBook Create service ->", data);
+        return callback(null, data);
+        // return (error) ? callback(error, null) : callback(null, data);
+      });
+    } catch (err) {
+      callback(err || "Some error occurred!", null);
     }
+  };
 
-    /**
-    * @description retrive all the Employee Data
-    * @param callback is data sent from Controller
-    * @return callback is used to callback Controller with data or error message
-    */
-    findAllContacts = (callback) => {
-        contactModel.findAllContacts((error, data) => {
-            return (error) ? callback(error, null) : callback(null, data);
-        });
+  /**
+   * @description retrive all the Employee Data
+   * @param callback is data sent from Controller
+   * @return callback is used to callback Controller with data or error message
+   */
+  findAllContacts = (callback) => {
+    try {
+      contactModel.findAllContacts((error, data) => {
+        return error ? callback(error, null) : callback(null, data);
+      });
+    } catch (err) {
+      callback(err || "Some error occurred!", null);
     }
+  };
 
-    /**
-    * @description retrive Employee Data
-    * @param objectId and callback is data sent from Controller
-    * @return callback is used to callback Controller with data or error message
-    */
-    findDataId = (contactObjectId, callback) => {
-        contactModel.findDataId(contactObjectId, (error, data) => {
-            logger.info("service->", data);
-            return (error) ? callback(error, null) : callback(null, data);
-        });
+  /**
+   * @description retrive Employee Data
+   * @param objectId and callback is data sent from Controller
+   * @return callback is used to callback Controller with data or error message
+   */
+  findDataId = (contactObjectId, callback) => {
+    try {
+      contactModel.findDataId(contactObjectId, (error, data) => {
+        logger.info("service->", data);
+        return error ? callback(error, null) : callback(null, data);
+      });
+    } catch (err) {
+      callback(err || "Some error occurred!", null);
     }
+  };
 
-    /**
-    * @description delete Contact Data
-    * @param ContactID and callback is data sent from Controller
-    * @return callback is used to callback Controller with or  without error message
-    */
-    deleteDataUsingId = (contactObjectId, callback) => {
-        contactModel.deleteDataUsingId(contactObjectId, error => {
-            return (error) ? callback(error) : callback(null);
-        });
+  /**
+   * @description delete Contact Data
+   * @param ContactID and callback is data sent from Controller
+   * @return callback is used to callback Controller with or  without error message
+   */
+  deleteDataUsingId = (contactObjectId, callback) => {
+    try {
+      contactModel.deleteDataUsingId(contactObjectId, (error) => {
+        return error ? callback(error) : callback(null);
+      });
+    } catch (err) {
+      callback(err || "Some error occurred!", null);
     }
+  };
 
-    /**
-    * @description Create method of Model is called to save the new Contact Data  Which also encrypts the password
-    * @param userdData is data sent from Controller
-    * @return callback is used to callback Controller
-    */
-    updateByID = (userId, newUserData, callback) => {
-        contactModel.updateById(userId, newUserData, (error, data) => {
-            logger.info("services->", data)
-            return (error) ? callback(error, null) : callback(null, data);
-        })
+  /**
+   * @description Create method of Model is called to save the new Contact Data  Which also encrypts the password
+   * @param userdData is data sent from Controller
+   * @return callback is used to callback Controller
+   */
+  updateByID = (userId, newUserData, callback) => {
+    try {
+      contactModel.updateById(userId, newUserData, (error, data) => {
+        logger.info("services->", data);
+        return error ? callback(error, null) : callback(null, data);
+      });
+    } catch (err) {
+      callback(err || "Some error occurred!", null);
     }
-
+  };
 }
 module.exports = new ContactService();
